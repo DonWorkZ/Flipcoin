208721627afc1a27fb93153039e7c3c3b48123de
Don <thequadengineer@gmail.com>
2020-10-05 07:55:00 +0300

    flip coin

---
+++ READMD.md
@@ -0,0 +1,1 @@
+Coin flipping, coin tossing, or heads or tails is the practice of throwing a coin in the air and checking which side is showing when it lands, in order to choose between two alternatives, sometimes used to resolve a dispute between two parties. It is a form of sortition which inherently has two possible outcomes.
---
+++ main.js
@@ -0,0 +1,52 @@
+const coin = {
+    state: 0,
+    flip: function() {
+
+        this.state = Math.floor(Math.random() * 2); 
+
+    },
+    toString: function() {
+            this.flip()
+            if (this.state === 0) 
+                return "Heads";  
+
+            return "Tails";                  
+    },
+    toHTML: function() {
+        const image = document.createElement('img');
+    
+        let img_src = this.toString()
+
+        image.src = img_src + ".png"
+        image.alt = img_src
+
+        image.width = "100";
+        image.height = "100";
+
+        document.getElementsByTagName('body')[0].appendChild(image);
+
+        return image;
+    }
+};
+
+function display20Flips() {
+    const results = [];
+    // 4. One point: Use a loop to flip the coin 20 times, each time displaying the result of the flip as a string on the page.  After your loop completes, return an array with the result of each flip.
+        for (let i = 0; i < 20; i++) {            
+            results[i] = coin.toString();
+            console.log(results[i])
+        }
+    }
+display20Flips();
+
+// draw image
+display20Images();
+function display20Images() {
+    console.log("------------------------This is displayImages()----------------------")
+    const results = [];
+    // 5. One point: Use a loop to flip the coin 20 times, and display the results of each flip as an image on the page.  After your loop completes, return an array with result of each flip.
+    for (let j = 0; j < 20; j ++){
+        results[j] = coin.toHTML()
+        console.log(results[j].alt);
+    }
+}
